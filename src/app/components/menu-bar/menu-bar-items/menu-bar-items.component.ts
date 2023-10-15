48cfd530b11a9e13080d5b05caeabcde317cd63a
JaqueBRSilva <j.silvabroccolo@gmail.com>
2023-10-14 21:57:18 -0300

    feat: add links do menu

--- src/app/components/menu-bar/menu-bar-items/menu-bar-items.component.ts
+++ src/app/components/menu-bar/menu-bar-items/menu-bar-items.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar-items',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-bar-items.component.css']
 })
 export class MenuBarItemsComponent implements OnInit {
+
+  @Input()
+  bioLink_Profile: string = 'https://bio.link/jaquebrsilva'
+
+  @Input()
+  Linkedin_Profile: string = 'https://www.linkedin.com/in/jaqueline-silva-broccolo/'
+
+  @Input()
+  GitHub_Profile: string = 'https://github.com/JaqueBRSilva'
 
   constructor() { }
 
